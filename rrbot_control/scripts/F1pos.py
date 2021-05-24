e3fd2d2291e6e56ba5fca937fdf04a40c9a610e2
Roberto <roberto1.schinelli@mail.polimi.it>
2021-05-24 18:14:44 +0200

    first file

---
+++ rrbot_control/scripts/F1pos.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import rospy
+from control_msgs.msg import JointControllerState
+from std_msgs.msg import Float64
+
+
+rospy.init_node('listenernode')
+
+pub_1 = rospy.Publisher('/simple_model/bridge_to_platform_position_controller/command', Float64, queue_size=10)
+pub_2 = rospy.Publisher('/simple_model/up_to_shuttle_position_controller/command', Float64, queue_size=10)
+
+rate=rospy.Rate(2)
+
+num = 0.7
+
+def callback(msg):
+    
+    global num    
+    num = msg.process_value
+ 
+rospy.Subscriber("/simple_model/bridge_to_platform_position_controller/state", JointControllerState, callback)
+
+
+num_2 = 0.7
+
+def callback_2(msg):
+    
+    global num_2    
+    num_2 = msg.process_value
+ 
+rospy.Subscriber("/simple_model/up_to_shuttle_position_controller/state", JointControllerState, callback_2)
+
+
+
+while (num < 0.2615 or num > 0.2635) or num_2 > -0.6115:  
+    print num
+    print num_2
+    position= 0.2625
+    rospy.loginfo(position) 
+    pub_1.publish(position) 
+    pub_2.publish(position-0.875)
+    rate.sleep()
